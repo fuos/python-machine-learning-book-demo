28b5e67e0a4e806d370f92576804a5389d4677ea
zkkio <34535312+zkkio@users.noreply.github.com>
2018-02-01 14:14:29 +0800

    Update AdalineGDdemo.py

--- code/ch02/AdalineGDdemo.py
+++ code/ch02/AdalineGDdemo.py
@@ -1,6 +1,34 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot class samples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
+                    alpha=0.8, c=cmap(idx),
+                    edgecolor='black',
+                    marker=markers[idx],
+                    label=cl)
 
 class AdalineGD(object):
 
@@ -8,7 +36,7 @@ class AdalineGD(object):
         self.eta = eta
         self.n_iter = n_iter
 
-    #批量梯度下降
+    #adalineGD 批量梯度下降
     def fit(self, X, y):
 
         self.w_ = np.zeros(1 + X.shape[1])
@@ -38,8 +66,7 @@ class AdalineGD(object):
         return np.where(self.activation(X) >= 0.0, 1, -1)
 
 
-df = pd.read_csv('http://archive.ics.uci.edu/ml/machine-learning-databases/iris/bezdekIris.data',
-                 header=None)
+df = pd.read_csv('http://archive.ics.uci.edu/ml/machine-learning-databases/iris/bezdekIris.data', header=None)
 
 y = df.iloc[0:100, 4].values
 y = np.where(y == 'Iris-setosa', -1, 1)
@@ -60,6 +87,34 @@ ax[1].plot(range(1, len(ada2.cost_) + 1), ada2.cost_, marker='o')
 ax[1].set_xlabel('Epochs')
 ax[1].set_ylabel('Sum-squared-error')
 ax[1].set_title('Adaline - Learning rate 0.0001')
+
+plt.tight_layout()
+
+plt.show()
+
+
+#特征标准化
+X_std = np.copy(X)
+X_std[:, 0] = (X[:, 0] - X[:, 0].mean()) / X[:, 0].std()
+X_std[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
+
+ada = AdalineGD(n_iter=15, eta=0.01)
+ada.fit(X_std, y)
+
+#判定区域
+plot_decision_regions(X_std, y, classifier=ada)
+plt.title('Adaline - Gradient Descent')
+plt.xlabel('sepal length [standardized]')
+plt.ylabel('petal length [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+
+plt.show()
+
+#迭代次数
+plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Sum-squared-error')
 
 plt.tight_layout()
 
